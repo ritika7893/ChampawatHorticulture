74c6bf99590fd9729c30551bb7c5ae39382f92c4
Ritika <ritikakumari87344@gmail.com>
2026-09-03 15:43:33 +0530

    updated

--- champawathorticul/asgi.py
+++ champawathorticul/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for govbillsystem project.
+ASGI config for champawathorticul project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'govbillsystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'champawathorticul.settings')
 
 application = get_asgi_application()
--- champawathorticul/wsgi.py
+++ champawathorticul/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for govbillsystem project.
+WSGI config for champawathorticul project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'govbillsystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'champawathorticul.settings')
 
 application = get_wsgi_application()
